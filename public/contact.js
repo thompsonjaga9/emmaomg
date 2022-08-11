a39106ae08e0d10bd85d97adb6e8a32612da5c93
temfa <topeakinfe@gmail.com>
2022-08-11 22:55:23 +0100

    Done

--- public/contact.js
+++ public/contact.js
@@ -6,7 +6,7 @@ window.onload = function () {
       // generate a five digit number for the contact_number variable
       this.contact_number.value = (Math.random() * 100000) | 0;
       // these IDs from the previous steps
-      emailjs.sendForm("service_yrziodf", "template_x0rk8yo", this).then(
+      emailjs.sendForm("service_jdr9aq4", "template_2j62v6r", this).then(
         function () {
           alert("Message sent Successfully!");
         },
